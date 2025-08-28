26ac709252d0039d75694226b47305642b1373f2
donniedice <donniedice@protonmail.com>
2025-08-28 03:44:39 -0500

    debug: add console logs to content.js for auto-skip

--- content.js
+++ content.js
@@ -1,27 +1,37 @@
 let autoSkipEnabled = false;
 
-chrome.storage.sync.get({ autoSkip: false }, (items) => {
+console.log('[RGXMods] content.js loaded.');
+
+chrome.storage.sync.get({ autoSkip: true }, (items) => {
   autoSkipEnabled = items.autoSkip;
+  console.log('[RGXMods] Initial autoSkip setting:', autoSkipEnabled);
 });
 
 chrome.storage.onChanged.addListener((changes, namespace) => {
   if (changes.autoSkip) {
     autoSkipEnabled = changes.autoSkip.newValue;
+    console.log('[RGXMods] autoSkip setting changed to:', autoSkipEnabled);
   }
 });
 
 const observer = new MutationObserver((mutations) => {
   if (autoSkipEnabled) {
+    console.log('[RGXMods] MutationObserver triggered, autoSkip is enabled.');
     for (const mutation of mutations) {
       for (const node of mutation.addedNodes) {
         if (node.nodeType === Node.ELEMENT_NODE) {
           const skipButton = node.querySelector('.ytp-ad-skip-button-modern, .ytp-ad-skip-button');
           if (skipButton) {
+            console.log('[RGXMods] Skip button found, attempting to click.');
             skipButton.click();
+          } else {
+            console.log('[RGXMods] Added node, but no skip button found.');
           }
         }
       }
     }
+  } else {
+    console.log('[RGXMods] MutationObserver triggered, autoSkip is disabled.');
   }
 });
 
@@ -32,9 +42,12 @@ observer.observe(document.body, {
 
 chrome.runtime.onMessage.addListener((request) => {
   if (request.action === "skipAd" && !autoSkipEnabled) {
+    console.log('[RGXMods] Hotkey pressed, autoSkip is disabled, attempting to click.');
     const skipButton = document.querySelector('.ytp-ad-skip-button-modern, .ytp-ad-skip-button');
     if (skipButton) {
       skipButton.click();
+    } else {
+      console.log('[RGXMods] Hotkey pressed, but no skip button found.');
     }
   }
 });
